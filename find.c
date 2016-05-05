99f88e8ace1f2aeee10193de83c3257ef0627ad9
yaowj <yaowj@localhost.localdomain>
2016-05-05 10:38:39 +0800

    fix find.c

--- find.c
+++ find.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
-
-int find(int a,int b){
-    int tmp;
-    if(a<b){
-        tmp = a;
-        a = b;
-        b = tmp;
+int IsEven(int a){
+    if(a%2) return 0;
+    else return 1;
+}
+int find(int x,int y){
+    if(x>y) return find(y,x);
+    if(y==0) return x;
+    else{
+        if(IsEven(x)){
+            if(IsEven(y))
+                return (find(x>>1,y>>1)<<1);
+            else
+                return find(x>>1,y);
+        }
+        else{
+            if(IsEven(y))
+                return find(x,y>>1);
+            else
+                return find(y,x-y);
+        }
     }
-    return a-b;
 }
